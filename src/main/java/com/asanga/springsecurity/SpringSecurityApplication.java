7861b280d0eae6f0ae16acfb1ca158ace75b3ee0
Asanga Bandara Wijekoon <asangawije@gmail.com>
2022-09-21 21:48:25 +1200

    iniital commit

---
+++ src/main/java/com/asanga/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,21 @@
+package com.asanga.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringSecurityApplication {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
